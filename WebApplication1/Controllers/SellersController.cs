0e392d48158ce244d7f3d9d8899b677d0ab554cc
Carlos Augusto <junior.rg27@gmail.com>
2022-08-10 18:07:56 -0300

    Update seller and custom service exception

--- WebApplication1/Controllers/SellersController.cs
+++ WebApplication1/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using WebApplication1.Models;
 using WebApplication1.Models.ViewModels;
 using System.Collections.Generic;
 using WebApplication1.Services.Exceptions;
+using System.Diagnostics;
 
 namespace WebApplication1.Controllers
 {
@@ -43,12 +44,12 @@ namespace WebApplication1.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error),new {message = "Id not provided"});
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
@@ -64,12 +65,12 @@ namespace WebApplication1.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
@@ -77,13 +78,13 @@ namespace WebApplication1.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
 
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             List<Department> departments = _departmentService.FindAll();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
@@ -95,23 +96,35 @@ namespace WebApplication1.Controllers
         {
             if (id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id is match" });
             }
             try
             {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (NotFoundException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+            catch (DbConcurrencyException e)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
 
             }
 
         }
+        public IActionResult Error(string message)
+		{
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier // pegar o ID interno da requisição
+
+
+            };
+            return View(viewModel);
+
+		}
     }
 }
--- WebApplication1/Models/ViewModels/ErrorViewModel.cs
+++ WebApplication1/Models/ViewModels/ErrorViewModel.cs
@@ -5,7 +5,8 @@ namespace WebApplication1.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+		public string Message { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
