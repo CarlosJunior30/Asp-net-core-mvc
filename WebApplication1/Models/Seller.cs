a74f9a0b35605db9efe7c39162309c5b99aee0af
Carlos Augusto <junior.rg27@gmail.com>
2022-08-11 14:05:09 -0300

    App localle, number and date formatting

--- WebApplication1/Models/Seller.cs
+++ WebApplication1/Models/Seller.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 
 {
+    
     public class Seller
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType ( DataType.EmailAddress)] // o framework transformou cade email em um Link de Email
         public string Email { get; set; }
+        
+        [Display (Name = "Birth Date")]// utilizado para custmizar o nome dos atributos da classe
+        [DataType (DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")] // utilizado para formatar a ordem da data
         public DateTime Birthday { get; set; }
+
+        [Display (Name = "Base Salary")]// utilizado para custmizar o nome dos atributos da classe
+        [DisplayFormat(DataFormatString = "{0:f2}")]//formata o modelo de valor para 2 casas decimais apos a virgula
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -3,15 +3,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Services;
+
 
 namespace WebApplication1
 {
@@ -47,6 +50,15 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,SeedingService seedingService)
         {
+            var enUs = new CultureInfo("en-US");//Opcoes de Lozalizações padrão dos estados unidos
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = new List<CultureInfo> { enUs },
+                SupportedUICultures = new List<CultureInfo> { enUs }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
