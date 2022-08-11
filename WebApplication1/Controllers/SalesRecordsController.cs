7973c0d37d9bf4b073d1037e26e784811601eff0
Carlos Augusto <junior.rg27@gmail.com>
2022-08-11 20:38:48 -0300

    Preparing sales search navigation views

---
+++ WebApplication1/Controllers/SalesRecordsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
